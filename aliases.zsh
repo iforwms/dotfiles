4fac8d38bb01dfa0d4226b2df2e1977b40d674fd
Ifor Waldo Williams <ifor@cors.tech>
2019-12-02 17:46:48 +0800

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,58 +1,72 @@
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
-alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-antwerp} }
+alias hosts="sudo vim /etc/hosts"
+# alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+# alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
+alias ll="ls -lah"
+weather() { curl -4 wttr.in/${1:-yangshuo} }
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
-alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 alias zbundle="antibody bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugins.sh"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias sites="cd $HOME/Sites"
-alias lara="sites && cd laravel/"
+alias code="cd $HOME/code"
 
 # Laravel
 alias a="php artisan"
 alias ams="php artisan migrate:fresh --seed"
+alias amm="php artisan make:model"
 
 # PHP
-alias php73="/usr/local/Cellar/php@7.3/7.3.12/bin/php"
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
+alias pu="vendor/bin/phpunit"
+alias pf="vendor/bin/phpunit --filter"
 
 # JS
-alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
-alias watch="npm run watch"
+alias nfresh="rm -rf node_modules/ package-lock.json && yarn install"
 
 # Vagrant
-alias v="vagrant global-status"
-alias vup="vagrant up"
-alias vhalt="vagrant halt"
-alias vssh="vagrant ssh"
-alias vreload="vagrant reload"
-alias vrebuild="vagrant destroy --force && vagrant up"
+function hs() {
+  (cd ~/Homestead && vagrant $*)
+}
+# alias v="vagrant global-status"
+# alias vup="vagrant up"
+# alias vhalt="vagrant halt"
+# alias vssh="vagrant ssh"
+# alias vreload="vagrant reload"
+# alias vrebuild="vagrant destroy --force && vagrant up"
 
 # Docker
-alias docker-composer="docker-compose"
-#alias dstop="docker stop $(docker ps -a -q)"
-#alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
-#alias dpurgeimages="docker rmi $(docker images -q)"
-#dbuild() { docker build -t=$1 .; }
-#dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+# alias docker-composer="docker-compose"
+# alias dstop="docker stop $(docker ps -a -q)"
+# alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
+# alias dpurgeimages="docker rmi $(docker images -q)"
+# dbuild() { docker build -t=$1 .; }
+# dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+
+# Yarn
+alias yr='yarn'
+alias yra='yarn add'
+alias yrs='yarn start'
+
+# Django
+alias pm="python manage.py"
+alias pi="pip install"
+alias p="python"
+alias sv="source venv/bin/activate"
 
 # Git
-alias commit="git add . && git commit -m"
-alias gcommit="git add . && git commit"
-alias amend="git commit --amend --no-edit"
-alias amendall="git add . && amend"
-alias wip="commit wip"
-alias gst="git status"
-alias gb="git branch"
-alias gc="git checkout"
-alias gd="git diff"
-alias resolve="git add . && git commit --no-edit"
-alias gl="git log --oneline --decorate --color"
-alias nuke="git clean -df && git reset --hard"
+alias gl='git log --oneline --all --graph --decorate --pretty=format:"%Cgreen%h %Cred%d %Creset%s  %Cblue(%ar) <%an>" $*'
+alias gs='git status'
+alias gc='git commit -m'
+alias gco='git checkout'
+alias gca='git commit -am'
+alias gcam='git commit --amend'
+alias gps='git push'
+alias gpl='git pull'
+alias ga='git add'
+alias nah='git reset --hard && git clean -df'
+# alias apgi='git ls-files -ci --exclude-standard -z | xargs -0 git rm --cached'
