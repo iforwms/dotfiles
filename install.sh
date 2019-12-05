d779ad714bb9355c649aceee130d6d94a960c086
Ifor Waldo Williams <ifor@cors.tech>
2019-12-05 12:38:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,31 +7,31 @@ if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Update Homebrew recipes
+echo "Update Homebrew recipes"
 brew update
 
-# Install all our dependencies with bundle (See Brewfile)
+echo "Install all our dependencies with bundle (See Brewfile)"
 brew tap homebrew/bundle
 brew bundle
 
-# Install PHP extensions with PECL
+echo "Install PHP extensions with PECL"
 pecl install memcached imagick
 
-# Install global Composer packages
+echo "Install global Composer packages"
 /usr/local/bin/composer global require laravel/installer
 
 # Install Laravel Valet
 # $HOME/.composer/vendor/bin/valet install
 
-# Create a Sites directory
+echo "Create a Code directory"
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
 mkdir $HOME/code
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+echo "Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles"
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
-# Symlink the Mackup config file to the home directory
+echo "Symlink the Mackup config file to the home directory"
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences
