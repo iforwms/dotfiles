42bc365b990b4a73207daa0e07b3b36018182655
Ifor Waldo Williams <ifor@cors.tech>
2019-12-11 09:13:42 +0800

    Update server-install.sh

--- server-install.sh
+++ server-install.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 echo "Setting up your Server..."
+sudo -v
 
-# echo "Updating repositories"
-# sudo apt update
+echo "Updating repositories"
+sudo apt update
 
-# echo "Upgrading packages"
-# sudo apt upgrade
+echo "Upgrading packages"
+sudo apt upgrade
 
 # TODO
 # Install Yarn, Node, Composer
 
 echo "Installing ZSH"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo apt install zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 echo "Setting ZSH as default shell"
 chsh -s $(which zsh)
@@ -22,11 +24,12 @@ rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 echo "Download ZSH plugins"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+mkdir -p ${ZSH_CUSTOM}/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 echo "Download Vim colour scheme"
-mkdir $HOME/.vim/colors
+mkdir -p $HOME/.vim/colors
 curl -o $HOME/.vim/colors/Benokai.vim 'https://raw.githubusercontent.com/benjaminwhite/Benokai/master/colors/Benokai.vim'
 
 echo "Removes .vimrc from $HOME (if it exists) and symlinks the .vimrc file from the .dotfiles"
