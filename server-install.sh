9a7dece530b0004de2226d1a8804bad8bef87aad
Ifor Waldo Williams <ifor@cors.tech>
2019-12-11 08:55:04 +0800

    Create server-install.sh

---
+++ server-install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Setting up your Server..."
+
+echo "Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles"
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+echo "Download ZSH plugins"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+echo "Download Vim colour scheme"
+mkdir $HOME/.vim/colors
+curl -o $HOME/.vim/colors/Benokai.vim 'https://raw.githubusercontent.com/benjaminwhite/Benokai/master/colors/Benokai.vim'
+
+echo "Removes .vimrc from $HOME (if it exists) and symlinks the .vimrc file from the .dotfiles"
+rm -rf $HOME/.vimrc
+ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
