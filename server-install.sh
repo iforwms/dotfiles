f3418d5cfedc17a06d7a513a1b49859db275fc81
Ifor Waldo Williams <ifor@cors.tech>
2019-12-11 09:19:13 +0800

    Update server-install.sh

--- server-install.sh
+++ server-install.sh
@@ -14,12 +14,13 @@ sudo apt upgrade
 
 echo "Installing ZSH"
 sudo apt install zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 echo "Setting ZSH as default shell"
 chsh -s $(which zsh)
 
-echo "Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles"
+echo "Creating symlink for .zshrc"
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
@@ -32,6 +33,6 @@ echo "Download Vim colour scheme"
 mkdir -p $HOME/.vim/colors
 curl -o $HOME/.vim/colors/Benokai.vim 'https://raw.githubusercontent.com/benjaminwhite/Benokai/master/colors/Benokai.vim'
 
-echo "Removes .vimrc from $HOME (if it exists) and symlinks the .vimrc file from the .dotfiles"
+echo "Creating symlink for .vimrc"
 rm -rf $HOME/.vimrc
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
