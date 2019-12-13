75417b5b0f7bc03302e9b9a0acbbaeb54afd2850
Ifor Waldo Williams <ifor@cors.tech>
2019-12-13 13:50:16 +0800

    Update path.zsh

--- path.zsh
+++ path.zsh
@@ -7,6 +7,7 @@ export PATH="$HOME/.yarn/bin:$PATH"
 # Use project specific binaries before global ones
 # export PATH="node_modules/.bin:vendor/bin:$PATH"
 
+export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # Make sure coreutils are loaded before system commands
