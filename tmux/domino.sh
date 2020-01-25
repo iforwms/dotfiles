b75fd2735d91adc8634de3965e0565f8b6f6641c
Ifor Waldo Williams <ifor@cors.tech>
2020-01-25 18:13:38 +0800

    Update domino.sh

--- tmux/domino.sh
+++ tmux/domino.sh
@@ -7,7 +7,7 @@ if ! tmux has-session -t domino; then
     tmux new -s domino -d -n DominoFrontend -c ~/code/domino/domino-api
 
     # Split window for main API development
-    tmux split-window -t domino:1 -v -p 80 -c ~/code/domino/domino-frontend
+    tmux split-window -t domino:1 -v -p 20 -c ~/code/domino/domino-frontend
 
     # Split window for watching CSS changes.
     tmux split-window -t domino:1.2 -h -p 66 -c ~/code/domino/domino-frontend
@@ -27,20 +27,26 @@ if ! tmux has-session -t domino; then
     # Start Homestead server.
     tmux send-keys -t domino:1.1 'hs up' Enter
 
-    # Create a new window called DominoAdmin
-    tmux new-window -t domino:2 -n DominoAdmin -c ~/code/domino/domino-admin
 
-    # Split window for main API development
-    tmux split-window -t domino:2 -v -p 20 -c ~/code/domino/domino-api
+    # Create a new window called DominoAdmin
+    tmux new-window -t domino:2 -n DominoAdmin -c ~/code/domino/domino-api
+
+    # Split window for admin
+    tmux split-window -t domino:2 -v -p 20 -c ~/code/domino/domino-admin
 
     # Split window for watching CSS changes.
-    tmux split-window -t domino:2.2 -h -p 50 -c ~/code/domino/domino-admin
+    tmux split-window -t domino:2.2 -h -p 66 -c ~/code/domino/domino-admin
+
+    tmux split-window -t domino:2.3 -h -p 50 -c ~/code/domino/domino-admin
 
     # Install any missing packages and start a server on port 3001.
-    tmux send-keys -t domino:2.2 'yr && PORT=3001 yrs' Enter
+    tmux send-keys -t domino:2.3 'yr && PORT=3001 yrs' Enter
 
     # Watch CSS file for changes.
-    tmux send-keys -t domino:2.1 'yr css' Enter
+    tmux send-keys -t domino:2.4 'yr css' Enter
+
+    # Get current git status.
+    tmux send-keys -t domino:2.2 'gs' Enter
 
     # Set active pane to main API pane on both wincows.
     tmux select-pane -t domino:1.1
