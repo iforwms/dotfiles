922980ebc27ee27cc07abb56519f02a23a89f21f
Ifor Waldo Williams <ifor@cors.tech>
2020-01-26 16:33:37 +0800

    Create cmus-notify.sh

---
+++ tmux/cmus-notify.sh
@@ -0,0 +1,19 @@
+# #!/bin/sh
+convertsecs() {
+    # h=$(bc <<< "${1}/3600")
+    ((m=(${1}%3600)/60))
+    ((s=${1}%60))
+    printf "%02d:%02d\n" $m $s
+    # printf "%02d:%02d:%02d\n" $h $m $s
+}
+
+title=$(cmus-remote -Q | grep 'tag title' | cut -d ' ' -f 3-)
+artist=$(cmus-remote -Q | grep 'tag artist' | cut -d ' ' -f 3-)
+year=$(cmus-remote -Q | grep 'tag date' | cut -d ' ' -f 3-)
+album=$(cmus-remote -Q | grep 'tag album' | cut -d ' ' -f 3-)
+duration=$(cmus-remote -Q | grep 'duration' | cut -d ' ' -f 2-)
+duration=$(convertsecs $duration)
+# title=$(cmus-remote -Q | grep tag | head -n 3 | sort -r | cut -d ' ' -f 3- | head -n 1)
+# album=$(cmus-remote -Q | grep tag | head -n 3 | sort -r | cut -d ' ' -f 3- | tail -n 1)
+# artist=$(cmus-remote -Q | grep tag | head -n 3 | sort -r | cut -d ' ' -f 3- | head -n 2 | tail -n 1)
+osascript -e "display notification \"${album//\"/\\\"}\" with title \"${title//\"/\\\"} (${year//\"/\\\"}) (${duration//\"/\\\"})\" subtitle \"${artist//\"/\\\"}\""
