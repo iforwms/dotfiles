e1210797f7965616c311e0b273b72b47bc742875
Ifor Waldo Williams <ifor@cors.tech>
2020-01-31 16:33:23 +0800

    Create colors.sh

---
+++ scripts/colors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ANSI Terminal Colors
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+# Modifiers
+# 0 - Normal Style
+# 1 - Bold
+# 2 - Dim
+# 3 - Italic
+# 4 - Underlined
+# 5 - Blinking
+# 7 - Reverse
+# 8 - Invisible
