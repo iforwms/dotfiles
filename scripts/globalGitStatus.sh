1d32ec1275391de3d9455e8cd2c54474661513cf
Ifor Waldo Williams <ifor@cors.tech>
2020-01-31 16:33:27 +0800

    add colors

--- scripts/globalGitStatus.sh
+++ scripts/globalGitStatus.sh
@@ -7,11 +7,11 @@ status=`git status -s`
 # If status, show details
 if [[ ! -z "$status" ]]; then
     echo
-    echo "Repo: "$repoName" ["$branchName"]"
+    echo -e "\033[1;32m" $repoName" \033[2;32m["$branchName"]\033[0;34m"
     echo
-    git status | head -2 | tail -1
+    git status | head -2 | tail -1 | awk '{print " " $0}'
     echo
     git status -s
     echo
-    echo "------"
+    echo "--"
 fi
