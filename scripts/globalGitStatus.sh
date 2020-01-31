f3eb3cef58bdb8a326b741fbd2731f56d1494f07
Ifor Waldo Williams <ifor@cors.tech>
2020-01-31 17:35:33 +0800

    Update globalGitStatus.sh

--- scripts/globalGitStatus.sh
+++ scripts/globalGitStatus.sh
@@ -7,7 +7,7 @@ status=`git status -s`
 # If status, show details
 if [[ ! -z "$status" ]]; then
     echo
-    echo -e "\033[1;32m" $repoName" \033[2;32m["$branchName"]\033[0;34m"
+    echo -e "\033[1;35m" $repoName" \033[2;35m["$branchName"]\033[0;37m"
     echo
     git status | head -2 | tail -1 | awk '{print " " $0}'
     echo
