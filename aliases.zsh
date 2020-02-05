15111566e38ef178c84c1dd1969a67725be2de20
Ifor Waldo Williams <ifor@cors.tech>
2020-02-05 21:58:05 +0800

    Add vim golf

--- aliases.zsh
+++ aliases.zsh
@@ -28,6 +28,7 @@ alias ping='ping -c 5'  # Pings with 5 packets, not unlimited
 alias df='df -h'        # Disk free, in gigabytes, not bytes
 alias du='du -h -c'     # Calculate total disk usage for a folder
 # alias zbundle="antibody bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugins.sh"
+alias vimgolf='docker run --rm -it -e "key=4ff4186b1f258b4dd2755c104835abeb" kramos/vimgolf'
 
 # Directories
 alias dot="cd $DOTFILES"
