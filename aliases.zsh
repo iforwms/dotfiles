d2e3c860978aee49ac4772088ddc3c345cd03bba
Ifor Waldo Williams <ifor@cors.tech>
2020-02-29 14:21:37 +0800

    temp disable docker shortcuts

--- aliases.zsh
+++ aliases.zsh
@@ -32,9 +32,9 @@ alias vimgolf='docker run --rm -it -e "key=***REDACTED***" kramos/vimgolf'
 
 # Docker
 alias doc="docker-compose"
-alias dstop="docker stop $(docker ps -a -q)"
-alias dpurgec="dstop && docker rm $(docker ps -a -q)"
-alias dpurgei="docker rmi $(docker images -q)"
+# alias dstop="docker stop $(docker ps -a -q)"
+# alias dpurgec="dstop && docker rm $(docker ps -a -q)"
+# alias dpurgei="docker rmi $(docker images -q)"
 # dbuild() { docker build -t=$1 .; }
 # dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
 
