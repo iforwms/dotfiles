7c8910110cad28f535241509b19bf3954c4bb452
Ifor Waldo Williams <ifor@cors.tech>
2020-03-04 15:00:28 +0800

    Add add all git alias

--- aliases.zsh
+++ aliases.zsh
@@ -107,6 +107,7 @@ alias gcam='git commit --amend'
 alias gps='git push'
 alias gpl='git pull'
 alias ga='git add'
+alias gaa='git add .'
 alias nah='git reset --hard && git clean -df'
 # alias apgi='git ls-files -ci --exclude-standard -z | xargs -0 git rm --cached'
 # find ~/code -mindepth 1 -maxdepth 4 -type d -name .git -execdir git status -s \;
