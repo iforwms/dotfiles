16127ebe76ae9ec3defbff3a2e1f63c404a79868
Ifor Waldo Williams <ifor@cors.tech>
2020-05-05 08:41:47 +0800

    Add alternative to raw git contnet

--- install/generic.sh
+++ install/generic.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RAW_GIT=https://raw.githubusercontent.com
+RAW_GIT=https://iforwms.com/music/setup
 echo "Setting up ZSH"
 rm -rf $HOME/.oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
@@ -19,24 +21,24 @@ rm -rf $HOME/.vim
 ln -s $HOME/.dotfiles/.vim $HOME/.vim
 
 echo "Installing php vim syntax highlighting"
-curl -fsSL -o $HOME/.dotfiles/.vim/syntax/php.vim https://raw.githubusercontent.com/StanAngeloff/php.vim/master/syntax/php.vim
+curl -fsSL -o $HOME/.dotfiles/.vim/syntax/php.vim $RAW_GIT/StanAngeloff/php.vim/master/syntax/php.vim
 
 echo "Installing vim color scheme"
-curl -fsSL -o $HOME/.dotfiles/.vim/colors/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim
-curl -fsSL -o $HOME/.dotfiles/.vim/autoload/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim
+curl -fsSL -o $HOME/.dotfiles/.vim/colors/onedark.vim $RAW_GIT/joshdick/onedark.vim/master/colors/onedark.vim
+curl -fsSL -o $HOME/.dotfiles/.vim/autoload/onedark.vim $RAW_GIT/joshdick/onedark.vim/master/autoload/onedark.vim
 
 echo "Creating symlink for .vimrc"
 rm -rf $HOME/.vimrc
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
 
 echo "Install tmux navigator vim plugin"
-curl -fsSL -o $HOME/.dotfiles/.vim/plugin/tmux_navigator.vim https://raw.githubusercontent.com/christoomey/vim-tmux-navigator/master/plugin/tmux_navigator.vim
+curl -fsSL -o $HOME/.dotfiles/.vim/plugin/tmux_navigator.vim $RAW_GIT/christoomey/vim-tmux-navigator/master/plugin/tmux_navigator.vim
 
 echo "Install vim surround plugin"
-curl -fsSL -o $HOME/.dotfiles/.vim/plugin/surround.vim https://raw.githubusercontent.com/tpope/vim-surround/master/plugin/surround.vim
+curl -fsSL -o $HOME/.dotfiles/.vim/plugin/surround.vim $RAW_GIT/tpope/vim-surround/master/plugin/surround.vim
 
 echo "Install vim commentary plugin"
-curl -fsSL -o $HOME/.dotfiles/.vim/plugin/commentary.vim https://raw.githubusercontent.com/tpope/vim-commentary/master/plugin/commentary.vim
+curl -fsSL -o $HOME/.dotfiles/.vim/plugin/commentary.vim $RAW_GIT/tpope/vim-commentary/master/plugin/commentary.vim
 
 echo "Creating symlink for .tmux.conf"
 rm -rf $HOME/.tmux.conf
