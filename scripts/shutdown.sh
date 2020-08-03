1d613ce3aa41755ac2e15b0936ca549a87d83999
Ifor Waldo Williams <ifor@cors.tech>
2020-08-03 12:49:37 +0800

    Add safe shutdown script

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -4,6 +4,7 @@ echo " Checking repos..."
 
 modifiedArray=()
 unpushed=()
+dirty=false
 
 while read line; do
     repoPath=`echo $line | sed "s/\/.git//"`
@@ -13,10 +14,16 @@ while read line; do
         modifiedArray+=( "$repoPath" )
     fi
 
+    unpushed=`git -C $repoPath cherry -v 2>/dev/null`
+    if [[ ! -z "$unpushed" ]]; then
+        unpushedArray+=( "$repoPath" )
+    fi
+
 done < <(find $HOME/code -mindepth 1 -maxdepth 4 -type d -name .git -prune)
 
 modifiedLength="${#modifiedArray[@]}"
 if ((modifiedLength > 0)); then
+    dirty=true
     echo
     echo -e "\033[1;35m" Found $modifiedLength modified repos:"\033[0;37m"
     echo
@@ -29,6 +36,7 @@ fi
 
 unpushedLength="${#unpushedArray[@]}"
 if ((unpushedLength > 0)); then
+    dirty=true
     echo
     echo -e "\033[1;35m" Found $unpushedLength unpushed repos:"\033[0;37m"
     echo
@@ -37,4 +45,9 @@ if ((unpushedLength > 0)); then
     do
         echo " " $i
     done
+fi
+
+if [[ ! -z "$dirty" ]]; then
+    echo "Nothing to push, shutting down..."
+    sudo shutdown -h now
 fi
