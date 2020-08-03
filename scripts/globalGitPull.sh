c1d36213ee3cc7b1e1c123b7bd5c4423d31f47dc
Ifor Waldo Williams <ifor@cors.tech>
2020-08-03 11:31:17 +0800

    update aliases

--- scripts/globalGitPull.sh
+++ scripts/globalGitPull.sh
@@ -4,14 +4,22 @@ repoName=`basename $(pwd)`
 branchName=`git rev-parse --abbrev-ref HEAD`
 status=`git status -s`
 
+echo
+echo -e "\033[1;35m" $repoName" \033[2;35m["$branchName"]\033[0;37m"
+echo
+
 # If files are modified, show details
 if [[ ! -z "$status" ]]; then
-    echo
-    echo -e "\033[1;35m" $repoName" \033[2;35m["$branchName"]\033[0;37m"
-    echo
     git status | head -2 | tail -1 | awk '{print " " $0}'
     echo
     git status -s
-    echo
-    echo "--"
+else
+    echo "No local changes."
 fi
+
+echo
+
+git pull
+
+echo
+echo "--"
