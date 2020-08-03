cd42312e93d9be6fac5ec55fb3e938d5a83c66cb
Ifor Waldo Williams <ifor@cors.tech>
2020-08-03 12:35:06 +0800

    add shutdown script - wip

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-echo "Checking for unpushed repos."
-# Search for any unpushed or unstaged changes in all repos
-# If unpushed at to array
-# If modified, at to another array
-# If any unpushed or modified, stop shutdown and list all repos
+echo " Checking repos..."
+
 modifiedArray=()
-# unpushed=()
+unpushed=()
 
 while read line; do
     repoPath=`echo $line | sed "s/\/.git//"`
 
-    echo
-    echo -e "\033[1;35m" Processing repo: $repoPath"\033[0;37m"
-    echo
-
-    # checkStatus $line
-    # modified+=$1
+    # echo -e "\033[1;35m" Processing repo: $repoPath"\033[0;37m"
 
     modified=`git -C $repoPath status -s`
     if [[ ! -z "$modified" ]]; then
         modifiedArray+=( "$repoPath" )
     fi
+
 done < <(find $HOME/code -mindepth 1 -maxdepth 4 -type d -name .git -prune)
 
-echo "${modifiedArray[@]}"
-# echo "${unpushed[@]}"
+modifiedLength="${#modifiedArray[@]}"
+if ((modifiedLength > 0)); then
+    echo
+    echo -e "\033[1;35m" Found $modifiedLength modified repos:"\033[0;37m"
+    echo
+
+    for i in "${modifiedArray[@]}"
+    do
+        echo " " $i
+    done
+fi
+
