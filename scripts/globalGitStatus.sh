f32e7602ad446708514d4d974d6495cacfca904c
Ifor Waldo Williams <ifor@cors.tech>
2020-08-03 16:39:32 +0800

    Refactor global git functions

--- scripts/globalGitStatus.sh
+++ scripts/globalGitStatus.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-repoName=`basename $(pwd)`
-branchName=`git rev-parse --abbrev-ref HEAD`
-status=`git status -s`
+repoPath=`echo $1 | sed "s/\/.git//"`
+repoName=`basename $repoPath`
+branchName=`git -C $repoPath rev-parse --abbrev-ref HEAD`
+status=`git -C $repoPath status -s`
 
 # If files are modified, show details
 if [[ ! -z "$status" ]]; then
     echo
     echo -e "\033[1;35m" $repoName" \033[2;35m["$branchName"]\033[0;37m"
     echo
-    git status | head -2 | tail -1 | awk '{print " " $0}'
+    git -C $repoPath status | head -2 | tail -1 | awk '{print " " $0}'
     echo
-    git status -s
+    git -C $repoPath status -s
     echo
     echo "--"
 fi
