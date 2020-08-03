0944ac7c47c60dbad6e3ff1c23a694835d9ffac7
Ifor Waldo Williams <ifor@cors.tech>
2020-08-03 16:39:32 +0800

    Refactor global git functions

--- scripts/globalGitPull.sh
+++ scripts/globalGitPull.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-repoName=`basename $(pwd)`
-branchName=`git rev-parse --abbrev-ref HEAD`
-status=`git status -s`
+repoPath=`echo $1 | sed "s/\/.git//"`
+repoName=`basename $repoPath`
+branchName=`git -C $repoPath rev-parse --abbrev-ref HEAD`
+status=`git -C $repoPath status -s`
 
 echo
 echo -e "\033[1;35m" $repoName" \033[2;35m["$branchName"]\033[0;37m"
@@ -10,16 +11,16 @@ echo
 
 # If files are modified, show details
 if [[ ! -z "$status" ]]; then
-    git status | head -2 | tail -1 | awk '{print " " $0}'
+    git -C $repoPath status | head -2 | tail -1 | awk '{print " " $0}'
     echo
-    git status -s
+    git -C $repoPath status -s
 else
     echo " No local changes."
 fi
 
 echo
 
-git pull
+git -C $repoPath pull
 
 echo
 echo "--"
