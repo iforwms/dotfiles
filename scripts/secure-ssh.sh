2953c3e9f082e8c16a6940ce2455d5b1adeb39ac
Ifor Waldo Williams <ifor@cors.tech>
2021-03-18 12:18:20 +0800

    Update secure ssh script

--- scripts/secure-ssh.sh
+++ scripts/secure-ssh.sh
@@ -1,20 +1,21 @@
-#/bin/bash
+#!/bin/bash
 
 echo "Current settings:"
-grep 'PermitRootLogin\|PasswordAuthentication\|ChallengeResponseAuthentication\|UsePam' /etc/ssh/sshd_config|grep -v '#'
-
-read -p "Do you want to update the sshd_config file? (y/n) " -n 1 -r
-
+grep -n 'PermitRootLogin\|PasswordAuthentication\|ChallengeResponseAuthentication\|UsePam' /etc/ssh/sshd_config|grep -v '#'
 echo
 
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Aborting..."
-    exit 1
+read -p "Do you want to update the sshd_config file? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+   echo
+   echo "Aborting..."
+   exit 1
 fi
 
+echo
 echo "Disabling root login and password login"
-sed '/^PermitRootLogin/s/yes/no/; /^PasswordAuthentication/s/yes/no/; /^ChallengeResponseAuthentication/s/yes/no/; /^UsePAM/s/yes/no/' /etc/ssh/sshd_config|sudo tee /etc/ssh/sshd_config
+sed '/^PermitRootLogin/s/yes/no/; /^PasswordAuthentication/s/yes/no/; /^ChallengeResponseAuthentication/s/yes/no/; /^UsePAM/s/yes/no/        ' /etc/ssh/sshd_config|sudo tee /etc/ssh/sshd_config >/dev/null
 
+echo
 echo "Secured settings:"
-grep 'PermitRootLogin\|PasswordAuthentication\|ChallengeResponseAuthentication\|UsePam' /etc/ssh/sshd_config|grep -v '#'
+grep -n 'PermitRootLogin\|PasswordAuthentication\|ChallengeResponseAuthentication\|UsePam' /etc/ssh/sshd_config|grep -v '#'
