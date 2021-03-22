263236461e07bdb679bc5e11c3ffa4247603ef56
Ifor Waldo Williams <ifor@cors.tech>
2021-03-22 14:03:04 +0800

    add initial fzf install script

---
+++ scripts/fzf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Installing FZF'
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+echo "Installing FZF checkout plugin"
+git clone https://github.com/stsewd/fzf-checkout.vim $HOME/.dotfiles/.vim/pack/default/start/fzf-checkout.vim
+
+echo 'Installing rust'
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo 'Installing ripgrep'
+cargo install ripgrep
