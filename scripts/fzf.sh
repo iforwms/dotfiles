d659f298964928f5711955c7cff31161d05306c0
Ifor Waldo Williams <ifor@cors.tech>
2021-03-22 15:38:35 +0800

    add fzf vim plugin

--- scripts/fzf.sh
+++ scripts/fzf.sh
@@ -4,6 +4,9 @@ echo 'Installing FZF'
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+echo "Installing FZF plugin"
+git clone https://github.com/junegunn/fzf.vim $HOME/.dotfiles/.vim/pack/default/start/fzf.vim
+
 echo "Installing FZF checkout plugin"
 git clone https://github.com/stsewd/fzf-checkout.vim $HOME/.dotfiles/.vim/pack/default/start/fzf-checkout.vim
 
