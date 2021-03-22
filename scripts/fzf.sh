1f40a488e4c98a246c1cf562d84f68c26de15cff
Ifor Waldo Williams <ifor@cors.tech>
2021-03-22 16:50:57 +0800

    fix fzf install script

--- scripts/fzf.sh
+++ scripts/fzf.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 echo 'Installing FZF'
+rm -rf $HOME/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 echo "Installing FZF plugin"
+rm -rf $HOME/.vim/pack/default/start/fzf
+rm -rf $HOME/.vim/pack/default/start/fzf.vim
+git clone https://github.com/junegunn/fzf.git ~/.vim/pack/default/start/fzf
 git clone https://github.com/junegunn/fzf.vim $HOME/.dotfiles/.vim/pack/default/start/fzf.vim
 
 echo "Installing FZF checkout plugin"
+rm -rf $HOME/.vim/pack/default/start/fzf-checkout.vim
 git clone https://github.com/stsewd/fzf-checkout.vim $HOME/.dotfiles/.vim/pack/default/start/fzf-checkout.vim
 
 echo 'Installing rust'
