6912565ecb9513629aab4b0cb3e43d94af56fa3d
Ifor Waldo Williams <ifor@cors.tech>
2021-03-24 12:33:21 +0800

    Improve loggin

--- scripts/fzf.sh
+++ scripts/fzf.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo 'Installing FZF'
+echo '[FZF] Installing FZF'
 rm -rf $HOME/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-echo "Installing FZF plugin"
+echo "[FZF] Installing FZF plugin"
 rm -rf $HOME/.vim/pack/default/start/fzf
 rm -rf $HOME/.vim/pack/default/start/fzf.vim
 git clone https://github.com/junegunn/fzf.git ~/.vim/pack/default/start/fzf
 git clone https://github.com/junegunn/fzf.vim $HOME/.dotfiles/.vim/pack/default/start/fzf.vim
 
-echo "Installing FZF checkout plugin"
+echo "[FZF] Installing FZF checkout plugin"
 rm -rf $HOME/.vim/pack/default/start/fzf-checkout.vim
 git clone https://github.com/stsewd/fzf-checkout.vim $HOME/.dotfiles/.vim/pack/default/start/fzf-checkout.vim
 
-echo 'Installing rust'
+echo '[FZF] Installing rust'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-echo 'Installing ripgrep'
+echo '[FZF] Installing ripgrep'
 cargo install ripgrep
