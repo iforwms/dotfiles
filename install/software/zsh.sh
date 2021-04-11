5ec36387606d6121a6aac7ac4b85ebf2b0ea60e1
Ifor Waldo Williams <ifor@cors.tech>
2021-04-11 21:21:47 +0800

    update zsh install

--- install/software/zsh.sh
+++ install/software/zsh.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-echo "[ZSH] Setting up..."
+ppi "Cloning repo..."
 rm -rf $HOME/.oh-my-zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+git clone --depth 1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
-echo "[ZSH] Creating symlink for .zshrc"
+ppi "Creating symlink for .zshrc"
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
-echo "[ZSH] Downloading plugins..."
+ppi "Downloading plugins..."
 rm -rf $HOME/.dotfiles/plugins/zsh-syntax-highlighting
 rm -rf $HOME/.dotfiles/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.dotfiles/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.dotfiles/plugins/zsh-autosuggestions
 
+ppi "Setting ZSH as default shell"
+sudo sh -c "ppi $(which zsh) >> /etc/shells" && chsh -s $(which zsh)
