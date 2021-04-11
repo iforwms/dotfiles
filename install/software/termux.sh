6521630f8e13283cb8442fd14d8fc142450c3702
Ifor Waldo Williams <ifor@cors.tech>
2021-04-11 18:06:27 +0800

    Split software install into individual scripts

--- install/software/termux.sh
+++ install/software/termux.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# Termux setup
-echo "Updating packages"
-pkg upgrade
+source $HOME/.dotfiles/scripts/pretty_print.sh
 
-echo "installing, xclip, tree, tmux, zsh, openssh, php, python, git, vim, yarn, nmap, proxychains"
-pkg install xclip tree tmux openssh zsh php python vim git yarn nmap proxychains-ng
+ppi "installing, xclip, tree, tmux, zsh, openssh, php, python, git, vim, yarn, nmap, proxychains"
+pkg install xclip tree openssh php python yarn nmap proxychains-ng
 
-echo "Setting zsh as default shell"
+ppi "Setting zsh as default shell"
 chsh -s zsh
 
-echo "Allow termux access to local storage"
+ppi "Allow termux access to local storage"
 termux-setup-storage
 
-echo "Symlink .gitconfig file to the home directory"
-rm -rf $HOME/.gitconfig
-ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
-
-echo "Symlinking termux config files"
+ppi "Symlinking termux config files"
 mkdir -p $HOME/.termux
 ln -s $HOME/.dotfiles/termux/colors.properties $HOME/.termux/colors.properties
 ln -s $HOME/.dotfiles/termux/termux.properties $HOME/.termux/termux.properties
--- install/software/vim.sh
+++ install/software/vim.sh
@@ -2,57 +2,59 @@
 
 RAW_GIT=https://raw.githubusercontent.com
 
-echo "[VIM] Setting up..."
+source $HOME/.dotfiles/scripts/pretty_print.sh
+
+ppi "Setting up..."
 rm -rf $HOME/.vim
 ln -s $HOME/.dotfiles/.vim $HOME/.vim
 
-echo "[VIM] Creating plugin directory"
+ppi "Creating plugin directory"
 mkdir -p $HOME/.dotfiles/.vim/pack/default/start
 
-echo "[VIM] Installing syntax highlighting plugin"
+ppi "Installing syntax highlighting plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/default/start/vim-polyglot
 git clone https://github.com/sheerun/vim-polyglot $HOME/.dotfiles/.vim/pack/default/start/vim-polyglot
 # vim -u NONE -c "helptags $HOME/.dotfiles/.vim/pack/default/start/vim-polyglot/doc" -c q
 
-echo "[VIM] Installing abolish plugin"
+ppi "Installing abolish plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/default/start/vim-abolish
 git clone https://github.com/tpope/vim-abolish $HOME/.dotfiles/.vim/pack/default/start/vim-abolish
 # vim -u NONE -c "helptags $HOME/.dotfiles/.vim/pack/default/start/vim-abolish/doc" -c q
 
-echo "[VIM] Installing surround plugin"
+ppi "Installing surround plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/default/start/vim-surround
 git clone https://github.com/tpope/vim-surround $HOME/.dotfiles/.vim/pack/default/start/vim-surround
 # vim -u NONE -c "helptags $HOME/.dotfiles/.vim/pack/default/start/vim-surround/doc" -c q
 
-echo "[VIM] Installing Wakatime plugin"
+ppi "Installing Wakatime plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/default/start/vim-wakatime
 git clone https://github.com/wakatime/vim-wakatime $HOME/.dotfiles/.vim/pack/default/start/vim-wakatime
 # vim -u NONE -c "helptags $HOME/.dotfiles/.vim/pack/default/start/vim-wakatime/doc" -c q
 
-echo "[VIM] Installing documentation plugin"
+ppi "Installing documentation plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/default/start/vim-doge
 git clone --depth 1 https://github.com/kkoomen/vim-doge $HOME/.dotfiles/.vim/pack/default/start/vim-doge
 # vim -u NONE -c "helptags $HOME/.dotfiles/.vim/pack/default/start/vim-doge/doc" -c q
 
-echo "[VIM] Installing rust plugin"
+ppi "Installing rust plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/plugins/start/rust.vim
 git clone https://github.com/rust-lang/rust.vim $HOME/.dotfiles/.vim/pack/plugins/start/rust.vim
 # vim -u NONE -c "helptags $HOME/.dotfiles/.vim/pack/default/start/vim-commentary/doc" -c q
 
-echo "[VIM] Installing commentary plugin"
+ppi "Installing commentary plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/default/start/vim-commentary
 git clone https://github.com/tpope/vim-commentary $HOME/.dotfiles/.vim/pack/default/start/vim-commentary
 # vim -u NONE -c "helptags $HOME/.dotfiles/.vim/pack/default/start/vim-commentary/doc" -c q
 
-echo "[VIM] Installing Vim Fugitive checkout plugin"
+ppi "Installing Vim Fugitive checkout plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/default/start/vim-fugitive
 git clone https://github.com/tpope/vim-fugitive $HOME/.dotfiles/.vim/pack/default/start/vim-fugitive
 
-echo "[VIM] Installing PHP import namespace plugin"
+ppi "Installing PHP import namespace plugin"
 rm -rf $HOME/.dotfiles/.vim/pack/default/start/vim-php-namespace
 git clone https://github.com/arnaud-lb/vim-php-namespace $HOME/.dotfiles/.vim/pack/default/start/vim-php-namespace
 
-echo "[VIM] Installing coc"
+ppi "Installing coc"
 COC_INSTALL=$HOME/.dotfiles/.vim/pack/coc/start
 rm -rf $COC_INSTALL
 mkdir -p $COC_INSTALL
@@ -60,22 +62,22 @@ curl -fsSL -o $COC_INSTALL/release.tar.gz https://github.com/neoclide/coc.nvim/a
 tar -xzf $COC_INSTALL/release.tar.gz
 # rm $COC_INSTALL/release.tar.gz
 
-echo "[VIM] Installing onedark color scheme"
+ppi "Installing onedark color scheme"
 rm -rf $RAW_GIT/joshdick/onedark.vim/master/colors/onedark.vim
 rm -rf $RAW_GIT/joshdick/onedark.vim/master/autoload/onedark.vim
 curl -fsSL -o $HOME/.dotfiles/.vim/colors/onedark.vim $RAW_GIT/joshdick/onedark.vim/master/colors/onedark.vim
 curl -fsSL -o $HOME/.dotfiles/.vim/autoload/onedark.vim $RAW_GIT/joshdick/onedark.vim/master/autoload/onedark.vim
 
-echo "[VIM] Installing FZF plugin"
+ppi "Installing FZF plugin"
 rm -rf $HOME/.vim/pack/default/start/fzf
 rm -rf $HOME/.vim/pack/default/start/fzf.vim
 git clone https://github.com/junegunn/fzf.git ~/.vim/pack/default/start/fzf
 git clone https://github.com/junegunn/fzf.vim $HOME/.dotfiles/.vim/pack/default/start/fzf.vim
 
-echo "[VIM] Installing FZF checkout plugin"
+ppi "Installing FZF checkout plugin"
 rm -rf $HOME/.vim/pack/default/start/fzf-checkout.vim
 git clone https://github.com/stsewd/fzf-checkout.vim $HOME/.dotfiles/.vim/pack/default/start/fzf-checkout.vim
 
-echo "[VIM] Creating symlink for .vimrc"
+ppi "Creating symlink for .vimrc"
 rm -rf $HOME/.vimrc
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
