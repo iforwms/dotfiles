afd8939d91a8f512df602d87ea90f33c5603ab9d
Ifor Waldo Williams <ifor@cors.tech>
2021-04-11 23:01:56 +0800

    move mac to to post install

--- install/software/post_install.sh
+++ install/software/post_install.sh
@@ -3,21 +3,52 @@
 source $HOME/.dotfiles/scripts/pretty_print.sh
 
 function post_install_mac() {
-  ppi "Post install mac"
+    ppi "Symlink Mackup config file and folder to the home directory"
+    rm -rf $HOME/.mackup.cfg
+    ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+    rm -rf $HOME/.mackup
+    ln -s $HOME/.dotfiles/mackup $HOME/.mackup
+
+    ppi "Setting up lilypond"
+    ln -s $HOME/.dotfiles/lilypond/lilypond-link /usr/local/bin/lilypond-link
+    chmod +x /usr/local/bin/lilypond-link
+
+    ppi "Cloning lilypond vim syntax highlighting"
+    git clone git@github.com:matze/vim-lilypond.git $HOME/.dotfiles/lilypond/vim
+
+    ppi "Installing tmux cmus plugin"
+    git clone https://github.com/Mpdreamz/tmux-cmus $HOME/.dotfiles/tmux/tmux-cmus
+
+    ppi "Setting up terminfo config"
+    ppw "Remember to update iterm (Preferences > Profiles > Terminal > Report Terminal Type: > xterm-256color-italic)"
+    tic $HOME/.dotfiles/tmux/256-italic-terminfo
+    tic -x $HOME/.dotfiles/tmux/tmux-terminfo
+
+    # Install python?
+    # ppi "Installing beets"
+    # pip3 install beets
+    # ppi "Setting up beets"
+    # mkdir -p $HOME/.config/beets
+    # rm -rf $HOME/.config/beets/config.yaml
+    # ln -s $HOME/.dotfiles/beets/config.yaml $HOME/.config/beets/config.yaml
+
+    ppi "Updating macOS preferences"
+    # We will run this last because this will reload the shell
+    source .macos
 }
 
 function post_install_linux() {
-  ppi "Post install linux"
+    ppi "Post install linux"
 }
 
 function post_install_android() {
-  ppi "Post install android"
+    ppi "Post install android"
 }
 
 if [[ $(uname -a|grep "Darwin") ]]; then
-  post_install_mac
+    post_install_mac
 elif [[ $(uname -a|grep "Android") ]]; then
-  post_install_android
+    post_install_android
 else
-  post_install_linux
+    post_install_linux
 fi
