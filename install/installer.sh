7d1b67a535310c2389aec9887cc68c30d447cf49
Ifor Waldo Williams <ifor@cors.tech>
2021-04-12 17:00:04 +0800

    Keep track of failed installs

--- install/installer.sh
+++ install/installer.sh
@@ -3,19 +3,23 @@
 source $HOME/.dotfiles/scripts/pretty_print.sh
 
 MISSING=""
+FAILED=""
 
 for COMMAND in "$@"
 do
     INSTALLER="$HOME/.dotfiles/install/software/$COMMAND.sh"
 
     if [[ -f $INSTALLER ]]; then
-        if command -v $COMMAND &> /dev/null
-        then
-            ppi "$COMMAND already installed, skipping"
-        else
+        # if command -v $COMMAND &> /dev/null
+        # then
+            # ppi "$COMMAND already installed, skipping"
+        # else
             ppi "Installing $COMMAND..."
-            $INSTALLER
-        fi
+            EXIT_CODE=$INSTALLER
+            if [[ $EXIT_CODE != 0 ]]; then
+                FAILED="${FAILED} $INSTALLER"
+            fi
+        # fi
     else
         MISSING="$COMMAND ${MISSING}"
     fi
@@ -23,4 +27,8 @@ done;
 
 if [[ $MISSING != "" ]]; then
     ppw "The installer for the following do not exist: $MISSING"
+fi
+
+if [[ $FAILED != "" ]]; then
+    ppw "Failed to install the following: $FAILED"
 fi
