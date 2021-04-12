4f4e01a327c0802c73a87461ff745e5228e93f8e
Ifor Waldo Williams <ifor@cors.tech>
2021-04-12 17:55:07 +0800

    Remove echo statements

--- install/software/r2.sh
+++ install/software/r2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "[r2] Creating symlink for .radare2rc"
+source $HOME/.dotfiles/scripts/pretty_print.sh
+
+ppi "Creating symlink for .radare2rc"
 rm -rf $HOME/.radare2rc
 ln -s $HOME/.dotfiles/.radare2rc $HOME/.radare2rc
 
--- install/software/wireshark.sh
+++ install/software/wireshark.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "Creating symlink for wireshark config"
+source $HOME/.dotfiles/scripts/pretty_print.sh
+
+ppi "Creating symlink for wireshark config"
 rm -rf $HOME/.config/wireshark
 ln -s $HOME/.dotfiles/wireshark $HOME/.config/wireshark
