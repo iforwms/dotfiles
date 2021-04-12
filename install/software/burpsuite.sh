0267fc9ea8b00f4a7eb89eb0915fe081545ddb62
Ifor Waldo Williams <ifor@cors.tech>
2021-04-12 08:02:04 +0800

    add burpsuite stub

--- install/software/burpsuite.sh
+++ install/software/burpsuite.sh
@@ -4,19 +4,19 @@ source $HOME/.dotfiles/scripts/pretty_print.sh
 
 if [[ $(uname -a|grep "Darwin") ]]; then
   ppi "Installing using Homebrew"
-  brew install python
+  brew install burpsuite
 elif [[ $(uname -a|grep "Android") ]]; then
   ppi "Installing using pkg"
-  pkg install python
+  pkg install burpsuite
 else
   ppi "Installing using apt"
-  sudo apt install python
+  sudo apt install burpsuite
 fi
 
 # TODO - Install from source
 function install_from_src() {
 }
 
-# ppi "Creating symlink for .python"
-# rm -rf $HOME/.python
-# ln -s $HOME/.dotfiles/.python $HOME/.python
+# ppi "Creating symlink for .burpsuite"
+# rm -rf $HOME/.burpsuite
+# ln -s $HOME/.dotfiles/.burpsuite $HOME/.burpsuite
