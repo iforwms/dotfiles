5fa240410410a0be34c77019e2ea8a149ba765e6
Ifor Waldo Williams <ifor@cors.tech>
2021-05-03 10:21:48 +0800

    Restart service after securing ssh

--- scripts/secure-ssh.sh
+++ scripts/secure-ssh.sh
@@ -19,3 +19,18 @@ sed '/^PermitRootLogin/s/yes/no/; /^PasswordAuthentication/s/yes/no/; /^Challeng
 echo
 echo "Secured settings:"
 grep -n 'PermitRootLogin\|PasswordAuthentication\|ChallengeResponseAuthentication\|UsePam' /etc/ssh/sshd_config|grep -v '#'
+
+read -p "Settings updated, you need to restart the ssh service for these changes to take effect, restart now? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+   echo
+   echo "Service not restarted."
+   exit 1
+else
+   echo
+   echo "Restarting ssh service"
+   sudo service ssh restart
+   echo "Service restarted."
+   exit 0
+fi
+
