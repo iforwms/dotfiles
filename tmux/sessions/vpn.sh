9d488ff47c0abe634fc42839f759e6e9612043ae
Ifor Waldo Williams <ifor@cors.tech>
2021-05-16 09:10:36 +0800

    update vpn session

--- tmux/sessions/vpn.sh
+++ tmux/sessions/vpn.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-## TODO: Make sure OS is MacOs
+source $HOME/.dotfiles/scripts/pretty_print.sh
+
+if [[ ! $(uname -a|grep "Darwin") ]]; then
+    ppe "Session must only be started on an iOS device."
+    exit 1
+fi
 
 ## Connect to VPN##
 if ! tmux has-session -t vpn; then
     # If not session called vpn create a window called vpn
-    tmux new -s vpn -d -n vpn -c ~/code
+    tmux new -s vpn -d -n vpn -c $HOME/code
 
-    tmux send-keys -t vpn:1.1 'lon' Enter
+    tmux send-keys -t vpn:1.1 'networksetup -setsocksfirewallproxy "Wi-Fi" localhost 2080 && ssh -D 2080 clients' Enter
 fi
 
 # Attach to vpn session.
