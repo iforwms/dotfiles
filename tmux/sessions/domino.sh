c52526bafe58ae6560b22b4ef64e658b571cf6f1
Ifor Waldo Williams <ifor@cors.tech>
2021-05-16 14:00:19 +0800

    tmux sessions - wip

--- tmux/sessions/domino.sh
+++ tmux/sessions/domino.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 
+source $HOME/.dotfiles/scripts/pretty_print.sh
+
 ## Setup dev environment for Domino Education ##
 if ! tmux has-session -t domino; then
-    # If not session called domino create a window called react
-    # in the frontend folder.
-    tmux new -s domino -d -n react -c ~/code/domino/domino-api
+    tmux new -s domino -d -n api -c ~/code/domino/domino-api
 
-    # Split window for main API development
-    tmux split-window -t domino:1 -v -p 20 -c ~/code/domino/domino-frontend
+    tmux split-window -t domino:1 -v -p 20 -c ~/code/domino/domino-api
 
-    # Split window for watching CSS changes.
-    tmux split-window -t domino:1.2 -h -p 66 -c ~/code/domino/domino-frontend
+    tmux new-window -t domino -n study ~/code/domino/domino-frontend
+    tmux split-window -t domino:1.2 -h -p 66 -c ~/code/domino/domino-api
+    tmux send-keys -t domino:1.3 'yr && PORT=3001 yrs' Enter
 
-    # Split window for Git.
-    tmux split-window -t domino:1.3 -h -p 50 -c ~/code/domino/domino-frontend
+#     # Watch CSS file for changes.
+#     tmux send-keys -t domino:1.4 'yr css' Enter
 
-    # Install any missing packages and start a server on port 3000.
-    tmux send-keys -t domino:1.3 'yr && PORT=3002 yrs' Enter
+#     # Run Git status.
+#     tmux send-keys -t domino:1.2 'gs' Enter
 
-    # Watch CSS file for changes.
-    tmux send-keys -t domino:1.4 'yr css' Enter
+#     # Start Homestead server.
+#     tmux send-keys -t domino:1.1 'hs up' Enter
 
-    # Run Git status.
-    tmux send-keys -t domino:1.2 'gs' Enter
-
-    # Start Homestead server.
-    tmux send-keys -t domino:1.1 'hs up' Enter
-
-    # Set active pane to main API pane on both wincows.
-    tmux select-pane -t domino:1.1
+#     # Set active pane to main API pane on both wincows.
+#     tmux select-pane -t domino:1.1
 fi
 
 # Attach to domino session.
-tmux a -t domino
+ppi "Domino session ready."
