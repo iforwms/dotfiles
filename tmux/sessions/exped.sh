19be19a8fe5ff7a81654b0289ef60942487a58d1
Ifor Waldo Williams <ifor@cors.tech>
2021-05-17 12:44:09 +0800

    Update exped tmux session

--- tmux/sessions/exped.sh
+++ tmux/sessions/exped.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
 
+source $HOME/.dotfiles/scripts/pretty_print.sh
+
 ## Setup dev environment for Exped Outdoor ##
 if ! tmux has-session -t exped; then
-    # If not session called exped create a window called react
-    # in the frontend folder.
-    tmux new -s exped -d -n react -c ~/code/exped/exped-api
-
-    # Split window for main API development
-    tmux split-window -t exped:1 -v -p 20 -c ~/code/exped/exped-frontend
-
-    # Split window for watching CSS changes.
-    tmux split-window -t exped:1.2 -h -p 66 -c ~/code/exped/exped-frontend
-
-    # Split window for Git.
-    tmux split-window -t exped:1.3 -h -p 50 -c ~/code/exped/exped-frontend
-
-    # Install any missing packages and start a server on port 3000.
-    tmux send-keys -t exped:1.3 'yr && PORT=3002 yrs' Enter
-
-    # Watch CSS file for changes.
-    tmux send-keys -t exped:1.4 'yr css' Enter
-
-    # Run Git status.
-    tmux send-keys -t exped:1.2 'gs' Enter
-
-    # Start Homestead server.
-    tmux send-keys -t exped:1.1 'hs up' Enter
-
-    # Set active pane to main API pane on both wincows.
-    tmux select-pane -t exped:1.1
+    tmux new -s exped -d -n vim -c ~/code/expednet
 fi
 
-# Attach to exped session.
-tmux a -t exped
+ppi "Exped session created."
