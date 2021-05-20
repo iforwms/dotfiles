30b8007e5c56fd8ae5f49ab0ef98516ef6298b76
Ifor Waldo Williams <ifor@cors.tech>
2021-05-20 17:29:27 +0800

    Add missing colon

--- tmux/sessions/chordgen.sh
+++ tmux/sessions/chordgen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Setup dev environment for Chordgen ##
-if ! tmux has-session -t chordgen then
+if ! tmux has-session -t chordgen; then
     tmux new -s chordgen -d -n vim -c ~/code/personal/chordgen
 fi
 
