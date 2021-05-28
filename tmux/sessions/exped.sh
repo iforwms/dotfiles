295a29ed35c5f3fe5c3c37e3722c9152036f25d5
Ifor Waldo Williams <ifor@cors.tech>
2021-05-28 12:04:42 +0800

    Add ssh window to exped session

--- tmux/sessions/exped.sh
+++ tmux/sessions/exped.sh
@@ -3,4 +3,9 @@
 ## Setup dev environment for Exped Outdoor ##
 if ! tmux has-session -t exped; then
     tmux new -s exped -d -n vim -c ~/code/expednet
+
+    tmux new-window -t exped -n server
+    tmux send-keys -t exped:2.1 'ssh exped' Enter
+
+    tmux select-pane -t exped:1.1
 fi
