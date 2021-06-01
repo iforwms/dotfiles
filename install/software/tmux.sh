5163926ae70cc1c9314491c9bec4b1c17629b87c
Ifor Waldo Williams <ifor@cors.tech>
2021-06-01 14:13:55 +0800

    Add tmux-ressurect

--- install/software/tmux.sh
+++ install/software/tmux.sh
@@ -13,6 +13,10 @@ else
   sudo apt-get install -y tmux
 fi
 
+ppi "Installing Tmux Resurrect"
+rm -rf $HOME/.dotfiles/tmux/plugins/tmux-resurrect
+git clone --depth 1 https://github.com/tmux-plugins/tmux-resurrect $HOME/.dotfiles/tmux/plugins/tmux-resurrect
+
 # TODO - Install from source
 function install_from_src() {
   ppi "Installing libevent"
