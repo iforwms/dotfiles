40cbf06082f8e4f4beae8f27f94bc23a7b7be3e3
Ifor Waldo Williams <ifor@cors.tech>
2021-08-23 12:09:30 +0800

    Add youtube alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -267,6 +267,7 @@ alias tcs=$DOTFILES/scripts/tmux_sessions.sh
 
 # Create PDF from markdown
 alias md2pdf=$DOTFILES/scripts/markdown2pdf.sh
+alias ytd=youtube-dl
 
 # Given a tmux session name, add suffixes until it is unique
 function _tmux_get_unique_id() {
