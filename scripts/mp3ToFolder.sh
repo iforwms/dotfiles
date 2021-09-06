b3ff8d7defc14f4fe8164eaf6cde9d0bd7499df3
Ifor Waldo Williams <ifor@cors.tech>
2021-09-07 00:28:30 +0800

    Add mp3ToFolder script

---
+++ scripts/mp3ToFolder.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Make sure to use find with $(pwd) to get the full path
+# of the files if using this script with multiple
+# files.
+
+properties="year disc date album track artist title disctotal tracktotal"
+
+for property in $properties; do
+    temp=$(ffprobe -loglevel quiet -show_entries format_tags="$property" "$1" | sed "1d;\$d;s/tag:${property}=//i")
+    declare ${property}="$temp"
+done
+
+echo "Finished setting vars"
+for property in $properties; do
+    echo ${property} - ${!property}
+done
+
+if [ -z "$album" -a "$album" == "" ]; then
+    album="Unknown - ${artist}"
+fi
+
+if [ -z "$title" -a "$title" == "" ]; then
+    echo "[ERROR] ${1} has no meta data, skipping..." | tee -a $HOME/Music/music_copying.log
+else
+    # TODO: Clean this mess up!
+    # dir="${artist}/[${year}] ${album}"
+    # dir="${artist}/${album}"
+
+    title="${title//\//_}"
+    artist="${artist//\//_}"
+    album="${album//\//_}"
+
+    dir="${album}"
+    full_dir="${HOME}/Music/downloads/${dir}"
+    track=$(echo $track|cut -d'/' -f1)
+    track="${track//\//_}"
+    track=$(printf %02d $track)
+    # filename="${track} - ${title}"
+    filename="${track} ${artist} - ${title}"
+    ext="${1##*.}"
+
+    mkdir -p "${full_dir}" 2>/dev/null
+
+    echo "[INFO] Copying ${1} to ${full_dir}" | tee -a $HOME/Music/music_copying.log
+    mv "$1" "${full_dir}/${filename}.${ext}"
+fi
+
