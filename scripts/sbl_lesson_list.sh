8c8165209e665e92ad84fbfe4e43da5b3ee5ca76
Ifor Waldo Williams <ifor@cors.tech>
2021-10-20 00:32:51 +0800

    Improve sbl download script

--- scripts/sbl_lesson_list.sh
+++ scripts/sbl_lesson_list.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TEMP_FILE=/tmp/sbl_page_source
+TEMP_DIR=$(mktemp -d)
 URL=$1
 SEASON=1
 
@@ -10,22 +11,39 @@ SEASON=$(printf "%02d" "$SEASON")
 
 wget -qO $TEMP_FILE "$URL"
 
+DOWNLOADS=$(
+    grep :downloads= $TEMP_FILE | \
+    sed 's/:downloads=//;s/"//g;s/&quot;/"/g;s/&amp;/and/g' | \
+    jq '.[].url' | \
+    tr -d '"'
+)
+
 FILES=$(
     grep init-course-modules $TEMP_FILE | \
-        sed 's/:init-course-modules=//;s/"//g;s/&quot;/"/g;s/&amp;/and/g;s/lesson [0-9]\+\( -\|:\) //ig' | \
-    jq '.[].items[].text' | \
-    sed 's/ /./g;s/://g'| \
-    tr -d '"' | tr "[:upper:]" "[:lower:]"
+    sed 's/:init-course-modules=//;s/"//g;s/&quot;/"/g;s/&amp;/and/g' | \
+    jq '.[].items[].uri'
 )
 
 I=0
 for F in $FILES; do
     ((I++))
+    COURSE=$(echo "${F}" | cut -d "/" -f 2 | tr "-" ".")
+    FILE=$(echo "${F}" | cut -d "/" -f 3 | sed 's/lesson [0-9]\+\( -\|:\) //ig' | tr "-" "." | tr -d '"')
     EPISODE=$(printf "%02d" "$I")
-    FILENAME="s${SEASON}e${EPISODE}.${F}"
-    touch "${FILENAME}"
-    echo "Created ${FILENAME}"
+    FILENAME="${COURSE}.s${SEASON}e${EPISODE}.${FILE}"
+    touch "${TEMP_DIR}/${FILENAME}"
+    echo "Created ${TEMP_DIR}/${FILENAME}"
 done
 
 echo
 echo "All files created!"
+
+for D in $DOWNLOADS; do
+    echo "Downloading ${D}"
+    wget -qP "${HOME}/Downloads" "$D"
+done
+
+echo
+echo "All files downloaded!"
+
+open -a "/Applications/Sublime Text.app" "$TEMP_DIR"
