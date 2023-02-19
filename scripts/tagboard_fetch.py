3099b52b1c6a5742fb9ffc7a07ebbbbba47b2b2f
Ifor Waldo Williams <ifor@cors.tech>
2023-02-19 22:03:20 +0800

    Update repo (2023/02/19 22:03).

--- scripts/tagboard_fetch.py
+++ scripts/tagboard_fetch.py
@@ -1,15 +1,13 @@
 from bs4 import BeautifulSoup
-import requests
+import requests, urllib.request
 
 debug = True
-
 fetch_url = 'https://tagboardeffects.blogspot.com/2023/01/t-rex-mudhoney-v11.html'
 test_file = "/Users/ifor/Downloads/Guitar FX Layouts T-Rex Mudhoney V1.1.html"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
 }
-
 
 if debug is True:
     print(f'[INFO] Opening {test_file}')
@@ -21,22 +19,26 @@ else:
     soup = BeautifulSoup(r.content, "html.parser")
 
 body = soup.find(class_ = 'post-body')
-name = soup.find(class_ = 'post-title').text
-intro = body.find_all('p')
 images = body.find_all('img')
+
+name = soup.find(class_ = 'post-title').text.strip()
+intro = body.find_all('p')
 image_anchors = [img.parent.attrs['href'] for img in images]
 effect_type = soup.find(class_ = 'post-labels').find('a').text
 
 print("Name:", name)
+print("Effect type:", effect_type)
 print("Intro text:", intro)
 print("Images:", image_anchors)
-print("Effect type:", effect_type)
+
+print(f'{name} | {effect_type}')
+
+for url in image_anchors:
+    print(url)
+    urllib.request.urlretrieve(url, 'abc')
 
 # 1. Make directory
 # 2. Save text
-# 3. Download images
 
 quit()
-
-print(soup)
 
