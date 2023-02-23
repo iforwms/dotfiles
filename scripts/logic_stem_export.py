641d8fcb6f3264d88ce66c81f0ea517c86697e5f
Ifor Waldo Williams <ifor@cors.tech>
2023-02-23 22:37:40 +0800

    Update repo (2023/02/23 22:37).

--- scripts/logic_stem_export.py
+++ scripts/logic_stem_export.py
@@ -5,6 +5,7 @@ import glob
 from mutagen.wave import WAVE
 
 debug = True
+# debug = False
 
 # Exporting files from Logic Pro
 # 1. Split all tracks into individual regions
@@ -18,115 +19,137 @@ debug = True
 #     Length as Time" and "Length as Absolute Position"
 # 11. Copy all marker info (Cmd + a, Cmd + c) and paste (Cmd + v) into a file called markers
 
+# Marker file example
+# 00:35:05:12.40  Song-1  00:03:30:12.40
+# 00:38:36:00.00  Song-2  00:02:32:16.53
+# 00:41:10:03.21  Song-3  00:04:15:05.05
+
 t0 = time.time()
 
-if len(sys.argv) < 2:
-    raise Exception("Missing directory path.")
-
-directory_path = sys.argv[1]
-output_directory = f"{directory_path}/output"
-markers_path = directory_path + "/markers"
+# Set global variables
+working_directory = None
+output_directory = None
+markers_path = None
 backup_path = None
 recording_date = None
 input_filetype = "wav"
 output_filetype = "mp3"
+sections = []
+stems = []
 
-original_files = []
-track_names = []
-file_durations = []
-section_names = []
+def preflight_checks():
+    global working_directory, output_directory, markers_path, backup_path
+    print("[STAGE] Checking arguments are valid.")
+    if len(sys.argv) < 2:
+        raise Exception("Missing directory path.")
 
-if not os.path.isdir(directory_path):
-    raise Exception("The specified directory does not exist.")
+    working_directory = sys.argv[1]
+    output_directory = f"{working_directory}/output"
+    markers_path = working_directory + "/markers"
 
-if not os.path.isfile(markers_path):
-    raise Exception("Missing 'markers' file.")
+    if not os.path.isdir(working_directory):
+        raise Exception("The specified directory does not exist.")
 
-if len(sys.argv) > 2:
-    backup_path = sys.argv[2]
+    if not os.path.isfile(markers_path):
+        raise Exception("Missing 'markers' file.")
 
-for file in os.listdir(directory_path):
-    if not file.endswith(f".{input_filetype}"):
-        continue
-    original_files.append(file)
+    if len(sys.argv) > 2:
+        backup_path = sys.argv[2]
 
-print(f"\n[STAGE] Validating {input_filetype.upper()} files...")
-for file in original_files:
-    filepath = f"{directory_path}/{file}"
+def check_raw_files():
+    global working_directory, input_filetype, recording_date, raw_files, stems
+    print(f"[STAGE] Validating {input_filetype.upper()} files...")
+    for file in os.listdir(working_directory):
+        if not file.endswith(f".{input_filetype}"):
+            continue
 
-    audio = WAVE(filepath)
-    duration = audio.info.length
+        filepath = f"{working_directory}/{file}"
+        stem = {
+            'name': file.split("_")[1].replace(f".{input_filetype}", ""),
+            'filename': file,
+            'filepath': filepath,
+            'duration': WAVE(filepath).info.length
+        }
+        stems.append(stem)
 
-    file_durations.append(duration)
+    recording_date = stems[0]['filename'].split("_")[0]
 
-if not all(i == file_durations[0] for i in file_durations):
-    raise Exception("File durations are not the same, aborting.")
+    if not all(i['duration'] == stems[0]['duration'] for i in stems):
+        raise Exception("File durations are not the same, aborting.")
 
-markers_file = open(markers_path, 'r')
-markers = markers_file.readlines()
-markers_regex = r"(\d{1,2}:\d{2}:\d{2}):(\d{2}).(\d{2})"
-markers_subst = "\\1.\\2\\3"
+def read_markers_file():
+    global markers_path, sections
+    print("[STAGE] Reading markers file.")
 
-recording_date=original_files[0].split("_")[0]
-instrument_track_count = len(original_files) * len(markers)
-
-print(f"\n[STAGE] Splitting {input_filetype.upper()} files by section markers...")
-split_track_count = 1
-for recording in original_files:
-    track_name = recording.split("_")[1].replace(f".{input_filetype}", "")
-    track_names.append(track_name)
-
-    for line in markers:
-        section_data = re.findall(r'\S+', line)
-        start_time = re.sub(markers_regex, markers_subst, section_data[0])
-        duration = re.sub(markers_regex, markers_subst, section_data[2])
+    markers_regex = r"(\d{1,2}:\d{2}:\d{2}):(\d{2}).(\d{2})"
+    markers_subst = "\\1.\\2\\3"
+    markers_file = open(markers_path, 'r')
+    markers = markers_file.readlines()
+    for marker in markers:
+        section_data = re.findall(r'\S+', marker)
         section_name = section_data[1]
-        section_dir_name = section_name.split("_")[0]
-        section_names.append(section_name)
+        section = {
+            'name': section_name,
+            'dir_name': section_name.split("-")[0],
+            'start_time': re.sub(markers_regex, markers_subst, section_data[0]),
+            'duration': re.sub(markers_regex, markers_subst, section_data[2])
+        }
+        sections.append(section)
 
-        new_directory_path = f"{output_directory}/{section_dir_name}/{input_filetype}s/{recording_date}"
-        new_filename = f"{recording_date}_{section_name}_{track_name}.{input_filetype}"
+def split_raw_files_by_marker():
+    global sections, stems, input_filetype, output_directory, recording_date, debug
+    print(f"[STAGE] Splitting {input_filetype.upper()} files by section markers.")
 
-        os.makedirs(new_directory_path, exist_ok=True)
+    split_track_count = 1
+    instrument_track_count = len(sections) * len(stems)
+    for stem in stems:
+        for section in sections:
+            new_directory_path = f"{output_directory}/{section['dir_name']}/{input_filetype}s/{recording_date}"
+            new_filename = f"{recording_date}_{section['name']}_{stem['name']}.{input_filetype}"
 
-        print(f"[INFO] [{split_track_count}/{instrument_track_count}] Creating file {new_filename}")
-        command = f"ffmpeg -y -nostdin -i '{directory_path}/{recording}' -ss '{start_time}' -t '{duration}' -c copy '{new_directory_path}/{new_filename}'" # 2>./error.log
-        if debug is False:
-            command += " -loglevel quiet"
+            os.makedirs(new_directory_path, exist_ok=True)
 
-        os.system(command)
-        split_track_count += 1
+            print(f"[INFO] [{split_track_count}/{instrument_track_count}] Creating file {new_filename}")
+            command = f"ffmpeg -y -nostdin -i '{stem['filepath']}' -ss '{section['start_time']}' -t '{section['duration']}' -c copy '{new_directory_path}/{new_filename}'" # 2>./error.log
 
-all_files_glob = f"{output_directory}/**/{input_filetype}s/{recording_date}/*.{input_filetype}"
-all_input_files = glob.glob(all_files_glob)
-section_names = [*set(section_names)] # Make section names unique
+            if debug is False:
+                command += " -loglevel quiet"
 
-print(f"\n[STAGE] Deleting empty {input_filetype.upper()}s...")
-for file in all_input_files:
-    # print(file)
-    # exit()
-    command = f"ffmpeg -i {file} -af silencedetect=noise=0.001 -f null - 2>&1 | awk '/silence_duration/{{print $8}}'"
-    # continue
+            os.system(command)
+            split_track_count += 1
 
-    silence_duration = subprocess.check_output(command, shell=True, encoding="UTF-8")
-    if silence_duration == '':
-        silence_duration = "0"
-    if silence_duration.__contains__("\n"):
-        silence_duration = silence_duration.split("\n")[-2]
-    silence_duration = int(float(silence_duration))
+def delete_empty_files():
+    global output_directory, input_filetype, recording_date
+    print(f"[STAGE] Purging empty {input_filetype.upper()}s.")
+    glob_pattern = f"{output_directory}/**/{input_filetype}s/{recording_date}/*.{input_filetype}"
+    stem_files = glob.glob(glob_pattern)
 
-    if silence_duration > 0:
-        audio = WAVE(file)
-        duration = int(audio.info.length)
+    for file in stem_files:
+        command = f"ffmpeg -i {file} -af silencedetect=noise=0.001 -f null - 2>&1 | awk '/silence_duration/{{print $8}}'"
 
-        if duration == silence_duration:
-            print(f"[INFO] [{file.split('/')[-1]}] {input_filetype.upper()} is silent, deleting.")
-            os.remove(file)
+        silence_duration = subprocess.check_output(command, shell=True, encoding="UTF-8")
+        if silence_duration == '':
+            silence_duration = "0"
+        if silence_duration.__contains__("\n"):
+            silence_duration = silence_duration.split("\n")[-2]
+        silence_duration = int(float(silence_duration))
 
-# Covert files
-all_input_files = glob.glob(all_files_glob)
-if output_filetype is not input_filetype:
-    print(f"\n[STAGE] Converting {input_filetype.upper()} files to {output_filetype.upper()}...")
+        if silence_duration > 0:
+            duration = int(WAVE(file).info.length)
+
+            if duration == silence_duration:
+                print(f"[INFO] [{file.split('/')[-1]}] {input_filetype.upper()} is silent, deleting.")
+                os.remove(file)
+
+def convert_files():
+    global input_filetype, output_filetype, debug, recording_date
+    if output_filetype is input_filetype:
+        print(f"[WARN] Input and output files are the same, skipping conversion.")
+        return
+
+    glob_pattern = f"{output_directory}/**/{input_filetype}s/{recording_date}/*.{input_filetype}"
+    all_input_files = glob.glob(glob_pattern)
+    print(f"[STAGE] Converting {input_filetype.upper()} files to {output_filetype.upper()}...")
     files_to_convert_count = len(all_input_files)
     current_file_count = 1
     for file in all_input_files:
@@ -141,77 +164,89 @@ if output_filetype is not input_filetype:
         os.system(command)
         current_file_count += 1
 
+def create_playalongs():
+    global sections, output_directory, recording_date, output_filetype
+    print("[STAGE] Creating play-along tracks.")
+    for directory in os.listdir(f"{output_directory}"):
+        if os.path.isfile(os.path.join(output_directory, directory)):
+            continue
 
-print("\n[STAGE] Creating play-along tracks...")
-total_mix_count = len(markers)
-current_mix_count = 1
-for directory in os.listdir(f"{output_directory}"):
-    if os.path.isfile(os.path.join(output_directory, directory)):
-        continue
-
-    song_directory_path = os.path.join(output_directory, directory) + f"/stems/{recording_date}"
-    # print(output_directory, song_directory_path)
-    # exit()
-    used_tracks = []
-    files = glob.glob(f"{song_directory_path}/*.{output_filetype}")
-    for file in files:
-        # TODO: Make into dictionary - folder name, file name (including take)
-        used_tracks.append(file.split("/")[-1].split("_")[-1].replace(f".{output_filetype}", ""))
-
-    for track in used_tracks:
-        command = "ffmpeg "
-        debug_log = []
-        track_count = 0
-        for file in files:
-            if file.endswith(f"{track}.{output_filetype}"):
+        input_directory_path = os.path.join(output_directory, directory) + f"/stems/{recording_date}"
+        for section in sections:
+            if section['dir_name'] != directory:
                 continue
 
-            command += f"-i {file} "
-            debug_log.append(file.split("/")[-1])
-            track_count += 1
+            section_tracks = []
+            for stem in stems:
+                input_filename = f"{recording_date}_{section['name']}_{stem['name']}.{output_filetype}"
+                if not os.path.isfile(f"{input_directory_path}/{input_filename}"):
+                    continue
+                output_filename = f"{recording_date}_{section['name']}_NO_{stem['name']}.{output_filetype}"
+                mix_name = f"{recording_date}_{section['name']}_MIX.{output_filetype}"
+                section_tracks.append({ 'filepath': f"{input_directory_path}/{input_filename}", 'filename': input_filename, 'playalong_name': output_filename, 'mix_name': mix_name })
 
-        print(f"[INFO] [{current_mix_count}/{total_mix_count}] Creating {track} play-along track for {directory}.")
-        playalong_dir = f"{song_directory_path}/../../playalongs/{recording_date}"
-        os.makedirs(playalong_dir, exist_ok=True)
-        command += f"-filter_complex amix=inputs={track_count}:duration=first {playalong_dir}/{recording_date}_{directory}_NO_{track}.{output_filetype}"
+            # Create Playalongs
+            current_playalong_tracks = 1
+            total_playalong_tracks = len(section_tracks)
+            for track_to_create in section_tracks:
+                command = "ffmpeg "
+                for track in section_tracks:
+                    if track['filename'] == track_to_create['filename']:
+                        continue
 
-        if debug is False:
-            command += " -loglevel quiet"
-        else:
-            debug_log.append(f"NO_{track}")
-            print(debug)
+                    command += f"-i {track['filepath']} "
 
-        os.system(command)
+                print(f"[INFO] [{current_playalong_tracks}/{total_playalong_tracks}] Creating {track_to_create['playalong_name']} play-along track for {directory}.")
+                playalong_dir = f"{input_directory_path}/../../playalongs/{recording_date}"
+                os.makedirs(playalong_dir, exist_ok=True)
+                command += f"-filter_complex amix=inputs={total_playalong_tracks - 1}:duration=first {playalong_dir}/{track_to_create['playalong_name']}"
 
-    # Create Main mix
-    command = "ffmpeg "
-    track_count = 0
-    for file in files:
-        command += f"-i {file} "
-        track_count += 1
+                if debug is False:
+                    command += " -loglevel quiet"
 
-    mix_dir = f"{song_directory_path}/../../mixes/{recording_date}"
-    os.makedirs(mix_dir, exist_ok=True)
-    print(f"[INFO] [{current_mix_count}/{total_mix_count}] Creating mix for {directory}.")
-    command += f"-filter_complex amix=inputs={track_count}:duration=first {mix_dir}/{recording_date}_{directory}_MIX.{output_filetype}"
+                current_playalong_tracks += 1
+                os.system(command)
 
-    if debug is False:
-        command += " -loglevel quiet"
+            # Create Mixes - TODO: Refactor
+            command = "ffmpeg "
+            for track in section_tracks:
+                command += f"-i {track['filepath']} "
 
+            print(f"[INFO] Creating mix for {directory}.")
+            mix_dir = f"{input_directory_path}/../../mixes/{recording_date}"
+            os.makedirs(mix_dir, exist_ok=True)
+            command += f"-filter_complex amix=inputs={total_playalong_tracks}:duration=first {mix_dir}/{track['mix_name']}"
+
+            if debug is False:
+                command += " -loglevel quiet"
+
+            current_playalong_tracks += 1
+            os.system(command)
+
+def backup_files():
+    global output_filetype, output_directory, backup_path
+    if not backup_path:
+        print("[WARN] No backup path specified, skipping backup.")
+        return
+
+    print("[STAGE] Uploading files to remove server.")
+    command = f"rsync --archive --recursive --verbose --include='*.{output_filetype}' --exclude='*.*' {output_directory}/ {backup_path}"
     os.system(command)
-    current_mix_count += 1
+    print("[SUCCESS] Backup complete.")
+
+preflight_checks()
+check_raw_files()
+read_markers_file()
+split_raw_files_by_marker()
+delete_empty_files()
+convert_files()
+create_playalongs()
 
 t1 = time.time() - t0
 
 print("[SUCCESS] Audio file generation complete, time elapsed (secs): ", t1)
 
-if backup_path:
-    print("\n[STAGE] Uploading files to remove server...")
-    command = f"rsync --archive --recursive --verbose --include='*.{output_filetype}' --exclude='*.*' {output_directory}/ {backup_path}"
-    print(command)
-    os.system(command)
-
-print("\n[SUCCESS] All done!\n")
+backup_files()
 
 exit()
 
