931d0a8d25a66767ef10691120040e36260b8559
Ifor Waldo Williams <ifor@cors.tech>
2023-10-10 21:37:21 +0800

    Quick commit - 2023/10/10 21:37

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -188,7 +188,13 @@ alias gd='git diff'
 alias gds='git diff --staged'
 alias grs='git restore --staged'
 alias nah='git reset --hard && git clean -df'
-alias gq="gaa; gc 'Quick commit - $(date "+%Y/%m/%d %H:%M")'; gpd"
+function gq() {
+  git add .
+  git commit -m "Quick commit - $(date '+%Y/%m/%d %H:%M')"
+  git push
+  ./deploy_remote
+}
+
 # alias apgi='git ls-files -ci --exclude-standard -z | xargs -0 git rm --cached'
 # find ~/code -mindepth 1 -maxdepth 4 -type d -name .git -execdir git status -s \;
 function ggs() {
