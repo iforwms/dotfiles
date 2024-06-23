4659a6d4c672a70e7df3461e265e0f3a749772d6
Ifor Waldo Williams <ifor@cors.tech>
2024-06-23 13:58:55 +0800

    Update repo (2024/06/23 13:58).

---
+++ scripts/termux-init.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+script_name="termux-init"
+packages_to_install=(
+  "ffmpeg"
+  "python"
+  "build-essential"
+  "file"
+  "curl"
+  "termux-api"
+  "zsh"
+  "git"
+  "imagemagick"
+  "jq"
+  "vim"
+)
+
+if ! [[ -x "$(command -v pkg)" ]]; then
+  echo "[${script_name}] This file must be run in Termux. Exiting..."
+  exit 1
+fi
+
+echo "[${script_name}] Updating repos..."
+pkg update
+
+echo "[${script_name}] Upgrading apps..."
+pkg upgrade
+
+echo "[${script_name}] Installing required apps..."
+for f in "${packages_to_install[@]}"; do
+  pgk install "$f"
+done
+
+pip install --upgrade pip
+
+termux-setup-storage
+
+echo "[${script_name}] Setting ZSH as default shell..."
+chsh -s zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+curl https://raw.githubusercontent.com/iforwms/dotfiles/master/scripts/yt > "${HOME}/yt"
+chmod +x "${HOME}/yt"
+
+
+echo "[${script_name}] All done!"
+exit 0
