4e20c4fdf92b49695c57f5e21feaf4bdc8f6ce82
Ifor Waldo Williams <ifor@cors.tech>
2024-06-24 07:58:21 +0800

    Quick commit - 2024/06/24 07:58

--- scripts/termux-init.sh
+++ scripts/termux-init.sh
@@ -5,20 +5,20 @@
 # Install all packages at once
 
 script_name="termux-init"
-packages_to_install=(
-  "ffmpeg"
-  "python"
-  "build-essential"
-  "file"
-  "curl"
-  "termux-api"
-  "zsh"
-  "git"
-  "imagemagick"
-  "jq"
-  "vim"
-)
-
+packages_to_install="
+  ffmpeg
+  python
+  build-essential
+  file
+  curl
+  termux-api
+  zsh
+  which
+  git
+  imagemagick
+  jq
+  vim
+"
 if ! [[ -x "$(command -v pkg)" ]]; then
   echo "[${script_name}] This file must be run in Termux. Exiting..."
   exit 1
@@ -31,25 +31,25 @@ echo "[${script_name}] Upgrading apps..."
 pkg upgrade
 
 echo "[${script_name}] Installing required apps..."
-for f in "${packages_to_install[@]}"; do
-  pkg install "$f"
-done
-
-# echo "[${script_name}] Upgrading pip..."
-# pip install --upgrade pip
+# for f in $packages_to_install; do
+   pkg install -y "$packages_to_install"
+# done
+# for f in "${packages_to_install[@]}"; do
+#   pkg install "$f"
+# done
 
 echo "[${script_name}] Creating symlinks..."
 termux-setup-storage
 
-echo "[${script_name}] Setting ZSH as default shell..."
-chsh -s zsh
+# echo "[${script_name}] Setting ZSH as default shell..."
+# chsh -s zsh
 
 # echo "[${script_name}] Installing OMZ..."
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "[${script_name}] Downloading iforwms yt script..."
-curl -fsSL https://dl.iforwms.com/downloads/yt > "${HOME}/yt"
-chmod +x "${HOME}/yt"
+yt_filepath=/data/data/com.termux/files/usr/bin/yt
+curl -fsSL https://dl.iforwms.com/yt > "$yt_filepath" && chmod +x "$yt_filepath"
 
 echo "[${script_name}] All done!"
 exit 0
