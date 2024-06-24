15a301c27168e2e688028f95916e7a71b4082999
Ifor Waldo Williams <ifor@cors.tech>
2024-06-24 14:06:59 +0800

    Quick commit - 2024/06/24 14:06

--- scripts/termux-init.sh
+++ scripts/termux-init.sh
@@ -64,7 +64,5 @@ echo "${GREEN}[${script_name}] Updating vi mode cursors...${NC}"
 echo "set vi-ins-mode-string \\1\\e[6 q\\2" > "${HOME}/.inputrc"
 echo "set vi-cmd-mode-string \\1\\e[2 q\\2" >> "${HOME}/.inputrc"
 
-echo "${GREEN}[${script_name}] Sourcing .bashrc...${NC}"
-
 echo "${GREEN}[${script_name}] All done!${NC}"
 exit 0
