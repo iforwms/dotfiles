28879f3475a1cc8e42c427611886e651af083015
Ifor Waldo Williams <ifor@cors.tech>
2026-01-19 08:19:24 +0800

    Update repo (2026/01/19 08:19).

---
+++ scripts/templates/laravel/maintenance.blade.php
@@ -0,0 +1,9 @@
+@extends('errors.layout')
+
+@section('message')
+  <div style="font-size: 24px">
+    <p>Server upgrade in progress, check back later.<p>
+    <p>更新正在进行中，请稍后再查看。</p>
+    <p>Serveruppgradering pågår, återkom senare.</p>
+  </div>
+@endsection
